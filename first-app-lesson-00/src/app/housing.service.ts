2828691b9db87b1c0de75f55124c43d071cb1020
Oskaras-coder <osafinas@gmail.com>
2024-03-21 14:20:53 +0200

    Push First_angular_project

---
+++ first-app-lesson-00/src/app/housing.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  // readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  
+  // housingLocationList: HousingLocation[] = [];
+  url = 'http://localhost:3000/locations';
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+  // getAllHousingLocations(): HousingLocation[] {
+  //   return this.housingLocationList;
+  // }
+  
+  // getHousingLocationById(id: number): HousingLocation | undefined {
+  //   return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  // }
+  
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+  }
+}
